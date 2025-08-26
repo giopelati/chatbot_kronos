75dc117ea434fedb520349a7965a4bfcb9c01bef
Giovanna Pelati <giovanna@pelati.com.br>
2025-08-26 08:34:23 -0300

    feat: adição do serviço de memória no agente RAG

--- agents/rag_agent.py
+++ agents/rag_agent.py
@@ -1,6 +1,7 @@
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
+from services.memory_service import get_memory
 from services.rag_service import retrieve_similar_docs
 
 # Conecta com o Gemini para geração de respostas
@@ -14,7 +15,7 @@ with open("prompts/rag_prompt.txt", "r", encoding="utf-8") as x:
     template = x.read()
 prompt = PromptTemplate.from_template(template)
 
-def run_rag_agent(query):
+def run_rag_agent(query, session_id):
     try:
         # Recupera os documentos mais relevantes como contexto
         context = retrieve_similar_docs(query)
@@ -24,8 +25,9 @@ def run_rag_agent(query):
         return "Desculpe, houve um problema ao processar sua pergunta. Tente novamente mais tarde.", ""
 
     try:
+        memory = get_memory(session_id)
         # Invoca a LLM
-        output = model.invoke(prompt.format(context=context, query=query))
+        output = model.invoke(prompt.format(context=context, query=query, memory=memory))
         return output.content, context
     
     except Exception as e:
