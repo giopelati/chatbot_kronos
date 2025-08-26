45c2d5f7cf9e8907606c4b82b4a457a2ddc16e22
Giovanna Pelati <giovanna@pelati.com.br>
2025-08-26 08:31:35 -0300

    feat: implementação do Structured Output

--- agents/guardrail_agent.py
+++ agents/guardrail_agent.py
@@ -1,31 +1,45 @@
-from ast import literal_eval
 import os
+from typing import Union
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
+from pydantic import BaseModel, Field
+from langchain.chains import LLMChain
+from services.memory_service import get_memory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
+class GuardrailOutput(BaseModel):
+    flag: int = Field(
+        description='0 se a entrada for válida, 1 se for ofensiva'
+    )
+    message: Union[str, None] = Field(
+        description='Mensagem educada para fugir do assunto caso flag=1, ou None se flag=0'
+    )
+    
 # Conecta com o Gemini para geração de respostas
 model = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
     google_api_key=os.getenv("GEMINI_API_KEY")
-)
+).with_structured_output(GuardrailOutput)
 
 # Lê o template do prompt
 with open("prompts/guardrail_prompt.txt", "r", encoding="utf-8") as x:
     template = x.read()
 prompt = PromptTemplate.from_template(template)
 
-def run_guardrail_agent(query):
-    try:
-        # Invoca a LLM
-        output = model.invoke(prompt.format(query=query))
-        
-        # Avalia o conteúdo do output como literal
-        parsed_output = literal_eval(output.content.strip())
+# Declara a pipeline
+pipeline = prompt | model 
 
-        if parsed_output == 0:
+def run_guardrail_agent(query, session_id):
+    try:
+        # Recupera a memória
+        memory = get_memory(session_id)
+
+        output: GuardrailOutput = pipeline.invoke({"query": query, "memory": memory})
+
+        if output.flag == 0:
             return True, None
-        elif isinstance(parsed_output, list) and parsed_output[0] == 1:
-            return False, parsed_output[1]
+        else:
+            return False, output.message
         
     except Exception as e:
         print(f"Erro no guardrail: {e}")
--- agents/judge_agent.py
+++ agents/judge_agent.py
@@ -1,31 +1,43 @@
-from ast import literal_eval
 import os
+from typing import Union
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
+from pydantic import BaseModel, Field
+from services.memory_service import get_memory
+
+class JudgeOutput(BaseModel):
+    flag: int = Field(
+        description='0 se a entrada for válida, 1 se for ofensiva'
+    )
+    message: Union[str, None] = Field(
+        description='Mensagem educada para fugir do assunto caso flag=1, ou None se flag=0'
+    )
 
 # Conecta com o Gemini para geração de respostas
 model = ChatGoogleGenerativeAI(
     model="gemini-2.0-flash",
     google_api_key=os.getenv("GEMINI_API_KEY")
-)
+).with_structured_output(JudgeOutput)
 
 # Lê o template do prompt
 with open("prompts/judge_prompt.txt", "r", encoding="utf-8") as x:
     template = x.read()
 prompt = PromptTemplate.from_template(template)
 
-def run_judge_agent(query, rag_output, context):
+# Declara a pipeline
+pipeline = prompt | model 
+
+def run_judge_agent(query, rag_output, context, session_id):
     try:
-        # Invoca a LLM
-        output = model.invoke(prompt.format(query=query, rag_output=rag_output, context=context))
+        # Recupera a memória
+        memory = get_memory(session_id)
+        
+        output: JudgeOutput = pipeline.invoke({"query": query, "rag_output": rag_output, "context": context, "memory": memory})
 
-        # Avalia o conteúdo do output como literal
-        parsed_output = literal_eval(output.content.strip())
-
-        if parsed_output == 0:
+        if output.flag == 0:
             return True, None
-        elif isinstance(parsed_output, list) and parsed_output[0] == 1:
-            return False, parsed_output[1]
+        else:
+            return False, output.message
         
     except Exception as e:
         print(f"Erro no juiz: {e}")
