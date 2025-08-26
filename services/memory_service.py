627bde06c52011f7cbc01dd94b15a81baeec8cae
Giovanna Pelati <giovanna@pelati.com.br>
2025-08-26 08:35:09 -0300

    refactor: remoção do ConversationBufferMemory

--- services/memory_service.py
+++ services/memory_service.py
@@ -2,11 +2,9 @@ from langchain_mongodb import MongoDBChatMessageHistory
 from langchain.memory import ConversationBufferMemory
 
 def get_memory(session_id):
-    chat_message_history = MongoDBChatMessageHistory(
+    return MongoDBChatMessageHistory(
         session_id=session_id,
         connection_string="mongodb://localhost:27017/",
         database_name="chatbot_kronos",
         collection_name="conversations",
     )
-    memory = ConversationBufferMemory(chat_memory=chat_message_history, return_messages=True)
-    return memory
